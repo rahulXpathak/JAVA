0718b547378d6df164da82d76130f33ce04bbc08
Rahul Pathak <137425116+rahulXpathak@users.noreply.github.com>
2024-11-02 07:14:04 +0000

    1st

---
+++ Fundamental/01. BasicsJava/02. TypeCasting/ExplicitTypeCasting.java
@@ -0,0 +1,26 @@
+class ExplicitTypeCasting{
+    public static void main(String args[]){
+        double a = 45.5;
+        int b;
+        b = (int)a;
+        System.out.println(b);
+
+
+
+        int n1 = 12; //we are use integer type data type so that when we performe permutation and combinataion between 2 intger result always integer 
+        int n2 = 5;
+        float res = n1/n2;
+        System.out.println(res);
+
+        float num1 = 45;
+        float num2 = 8;
+
+        double result = num1 / num2;
+        
+        System.out.println(result);
+
+
+
+
+    }
+}
---
+++ Fundamental/01. BasicsJava/02. TypeCasting/implicitTypeCasting.java
@@ -0,0 +1,9 @@
+class implicitTypeCasting{
+    public static void main(String args[]){
+        int a = 45;
+        double b;
+        b = a;
+        System.out.println(b);
+    }
+
+}
